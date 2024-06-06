b035c458f033d7a45b80d8bf0c54c7f760e38030
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-06-06 18:56:05 -0400

    informacion y seleccion personajes

---
+++ src/data/consts.ts
@@ -0,0 +1,39 @@
+export const IMAGESLOCATION="/images/chars"
+export const SELECTED_CHARS_KEY="matchCharacters"
+
+export const CHARACTERS={
+    p1:[
+        {
+            name:"m4a1",
+            anims:["idle.png","die.png"],
+            player:"p1",
+            scale: 0.4,
+        },
+        {
+            name:"cyclops",
+            anims:["idle.png","die.png"],
+            player:"p1"
+        },
+        {
+            name:"mina",
+            anims:["idle.gif"],
+            player:"p1"
+        },
+
+    ],
+    p2:[
+        {
+            name:"kalisa",
+            anims:["idle.gif"],
+            player:"p2",
+            scale:0.5
+        },
+        {
+            name:"kyubey",
+            anims:["idle.gif"],
+            player:"p2",
+            scale:0.1,
+        },
+        
+    ]
+}
